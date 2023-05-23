9a4a3399ca849df78d6de1e6a32788772c36dbb8
Andrei-Chiorian <achiorian23@yahoo.com>
2023-05-23 19:09:40 +0200

    23/05/2023 19:00

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/**/*.blade.php",
+    "./resources/**/*.blade.php",    
     "./resources/**/*.js",
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
   ],
